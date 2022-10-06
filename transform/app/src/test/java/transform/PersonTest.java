6e1f2667071824ed8e56982b4128563902b89cc1
Gabriel Lopes Veiga <gabriellopvei@gmail.com>
2022-10-06 14:51:23 -0300

    More tests

--- transform/app/src/test/java/transform/PersonTest.java
+++ transform/app/src/test/java/transform/PersonTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.Instant;
 
-class XmlGeneratorTest {
+class PersonTest {
     @Test
     void equalsId() {
         final var person1 = new Person("cpf", "name", 18);
@@ -97,5 +97,39 @@ class XmlGeneratorTest {
         final var person1 = new Person("cpf", "name", 18);
 
         assertEquals("name (cpf), 18 years old", person1.toString());
+    }
+
+    static class BuilderTest {
+        @Test
+        void withCpf() {
+            final var sut = new Person.Builder().withCpf("1234");
+            final var expected = new Person("1234", null, 0);
+
+            assertEquals(expected, sut.build());
+        }
+
+        @Test
+        void withName() {
+            final var sut = new Person.Builder().withName("name");
+            final var expected = new Person(null, "name", 0);
+
+            assertEquals(expected, sut.build());
+        }
+
+        @Test
+        void withAge() {
+            final var sut = new Person.Builder().withAge(20);
+            final var expected = new Person(null, null, 20);
+
+            assertEquals(expected, sut.build());
+        }
+
+        @Test
+        void complete() {
+            final var sut = new Person.Builder().withCpf("1234").withName("name").withAge(20);
+            final var expected = new Person("1234", "name", 20);
+
+            assertEquals(expected, sut.build());
+        }
     }
 }
